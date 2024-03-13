d903b2dde880d7c8b7509c2d281292ccbe173c8a
Robert Ferguson <robertferguson78@gmail.com>
2024-03-13 16:39:00 +0000

    update to todo list - break into seperate files - fixed todoText not re-setting

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,23 @@
 <script>
+  import Title from "./Title.svelte";
+  import AddTodoForm from "./AddTodoForm.svelte";
+  import TodoList from "./TodoList.svelte";
   import { v4 as uuidv4 } from 'uuid';
+  import DoneList from "./DoneList.svelte";
   
   let todoText = "";
   let todoItems = [];
   let doneItems = [];
 
-  function addTodo() {
+  function addTodo(todoText) {
     const todo = {
       text: todoText,
       date: new Date().toLocaleString("en-IE"),
+      id: uuidv4()
     };
     todoItems.push(todo);
     todoItems = [...todoItems];
-    todoText = "";
   }
-
-  const todo = {
-    text: todoText,
-    date: new Date().toLocaleString("en-IE"),
-    id: uuidv4()
-  };
 
   function markTodo(id) {
     const found = todoItems.findIndex((todo) => todo.id == id);
@@ -41,65 +39,10 @@
 
 
 <div class="container">
-  <div class="box has-text-centered">
-    <div class="title"> Simple Todo List</div>
-    <div class="subtitle">Fun things to do</div>
-  </div>
+  <Title title="Simple Todo List" subTitle="Fun Things to do"/>
   <div class="section box">
-    <div class="field is-horizontal">
-      <div class="field-label is-normal">
-        <label for="todo" class="label">What should I do?</label>
-      </div>
-      <div class="field-body">
-        <div class="field">
-          <p class="control">
-            <input bind:value={todoText} id="todo" class="input" type="text" placeholder="Type something...">
-          </p>
-        </div>
-        <button on:click={addTodo} class="button">Add Todo</button>
-      </div>
-    </div>
+    <AddTodoForm addTodo="{addTodo}" />
   </div>
-  <div class="section box">
-    <div class="title is-6">Things yet do</div>
-    <table class="table is-fullwidth">
-      <thead>
-        <th>Task</th>
-        <th>Date</th>
-        <th>Action</th>
-      </thead>
-      <tbody>
-        {#each todoItems as todo}
-          <tr>
-            <td> {todo.text} </td>
-            <td> {todo.date} </td>
-            <td>
-              <button on:click={markTodo(todo.id)} class="button">Mark Done</button>
-            </td>
-          </tr>
-        {/each}
-      </tbody>
-    </table>
-  </div>
-  <div class="section box">
-    <div class="title is-6">Things Done</div>
-    <table class="table is-fullwidth">
-      <thead>
-        <th>Task</th>
-        <th>Date</th>
-        <th>Action</th>
-      </thead>
-      <tbody>
-        {#each doneItems as todo}
-          <tr>
-            <td> {todo.text} </td>
-            <td> {todo.date} </td>
-            <td>
-              <button on:click={deleteTodo(todo.id)} class="button">Delete</button>
-            </td>
-          </tr>
-        {/each}
-      </tbody>
-    </table>
-  </div>
+  <TodoList caption="Items Todo" items="{todoItems}" deleteHandler="{markTodo}"/>
+  <DoneList caption="Items Done" items="{doneItems}" deleteHandler="{deleteTodo}"/>
 </div>
---
+++ src/Title.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let title;
+    export let subTitle;
+  </script>
+  <div class="box has-text-centered">
+    <div class="title"> {title} </div>
+    <div class="subtitle"> {subTitle} </div>
+  </div>
+  
